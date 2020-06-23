f81d0221f28b8676ca0e68bba119e43df562f331
SinazoBogicevic <47321511+SinazoBogicevic@users.noreply.github.com>
2020-06-23 14:53:28 +0200

    Updates

---
+++ precache-manifest.710791446a50289d76e9d7cb1a54ab34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3130631a24f8e6d77b2dd223b8780f2",
+    "url": "/movie-search-engine/index.html"
+  },
+  {
+    "revision": "168aa8ab940ad9a083de",
+    "url": "/movie-search-engine/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dbb4c0fdd86edd883efe",
+    "url": "/movie-search-engine/static/js/2.120c8ef4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search-engine/static/js/2.120c8ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "168aa8ab940ad9a083de",
+    "url": "/movie-search-engine/static/js/main.c662ea5e.chunk.js"
+  },
+  {
+    "revision": "a43b67178d88648cc3f2",
+    "url": "/movie-search-engine/static/js/runtime-main.5eaacbe9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-search-engine/static/media/logo.5d5d9eef.svg"
+  }
+]);
